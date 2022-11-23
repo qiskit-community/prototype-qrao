47dfd67de3c7add69b1e98ddc1a8615c9b6c3dc2
garrison <garrison@users.noreply.github.com>
2022-11-23 23:03:18 +0000

    deploy: e8494b7edded6fea1f263139177fb5028e300546

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev1+g107666d',
+    VERSION: '0.1.dev1+ge8494b7',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
